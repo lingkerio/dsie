c1a718d73f1b9ab0c78ad8438bd8603ff5766fa7
showlibia <frunnever@gmail.com>
2024-08-29 00:56:56 +0800

    update Go-version Makefile

--- BST/AVL/showlibia/Go-version/main.go
+++ BST/AVL/showlibia/Go-version/main.go
@@ -5,14 +5,11 @@ import (
 )
 
 func main() {
-	// 定义命令行参数
 	runTest := flag.Bool("test", false, "Run the test function")
 	runCorrect := flag.Bool("correct", false, "Run the correct function")
 
-	// 解析命令行参数
 	flag.Parse()
 
-	// 根据命令行参数执行对应的函数
 	if *runTest {
 		test()
 	}
@@ -21,7 +18,6 @@ func main() {
 		correct()
 	}
 
-	// 如果没有提供任何参数，默认执行两个函数
 	if !*runTest && !*runCorrect {
 		test()
 		correct()
