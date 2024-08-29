cfadff93a6ebb17f032f6fb50da80c42df376d2f
lingkerio <lingkerio@qq.com>
2024-08-29 22:26:45 +0800

    update correct.sh

--- BST/correctness/correct.sh
+++ BST/correctness/correct.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXPECTED_HASH="cef681ae08c9896b1e5ee282358f2605e06a255ca230985dd0b65147c2fa13c3"
+EXPECTED_HASH="790fa2b9e213216160a6cefc1581c9e22c98495778b94bc70f149d3e280e3ecc" # 4138092 dataset
 
 # 文件路径
 FILE="./correctness.csv"
